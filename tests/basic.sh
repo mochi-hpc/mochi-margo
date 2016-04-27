c44dc05b80fc38f9e0c6799ceff1333036688a45
Phil Carns <carns@mcs.anl.gov>
2016-04-27 11:43:52 -0400

    automated test for client/server

---
+++ tests/basic.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+
+if [ -z $srcdir ]; then
+    echo srcdir variable not set.
+    exit 1
+fi
+source $srcdir/tests/test-util.sh
+
+# start 1 server with 2 second wait, 20s timeout
+test_start_servers 1 2 20
+
+sleep 1
+
+#####################
+
+# run client test, which will also shut down server when done
+run_to 10 examples/client $svr1 &> /dev/null 
+if [ $? -ne 0 ]; then
+    wait
+    exit 1
+fi
+
+#####################
+
+
+wait
+
+echo cleaning up $TMPBASE
+rm -rf $TMPBASE
+
+exit 0
---
+++ tests/test-util.sh
@@ -0,0 +1,40 @@
+#
+# General test script utilities
+#
+
+
+function run_to ()
+{
+    maxtime=${1}s
+    shift
+    timeout --signal=9 $maxtime "$@"
+}
+
+function test_start_servers ()
+{
+    nservers=${1:-4}
+    startwait=${2:-15}
+    maxtime=${3:-120}s
+    repfactor=${4:-0}
+    pid=$$
+    startport=3344
+    endport=`expr 3344 + $nservers - 1`
+
+    # start daemons
+    for i in `seq $startport $endport`
+    do
+        timeout --signal=9 ${maxtime} examples/server tcp://localhost:$i single &
+        if [ $? -ne 0 ]; then
+            # TODO: this doesn't actually work; can't check return code of
+            # something executing in background.  We have to rely on the
+            # return codes of the actual client side tests to tell if
+            # everything started properly
+            exit 1
+        fi
+    done
+
+    # wait for servers to start
+    sleep ${startwait}
+
+    svr1="tcp://localhost:$startport"
+}
