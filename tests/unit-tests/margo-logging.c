d6b01dda7705bc42c027a12dae947e631f6926d8
Phil Carns <carns@mcs.anl.gov>
2021-06-28 17:45:48 -0400

    test case for varying log level

--- tests/unit-tests/margo-logging.c
+++ tests/unit-tests/margo-logging.c
@@ -96,9 +96,73 @@ static MunitResult default_log_level(const MunitParameter params[], void* data)
     return MUNIT_OK;
 }
 
+static char* log_level_params[] = {"trace", "debug", "info", "warning", "error", "critical", NULL};
+static MunitParameterEnum get_log_level[] = {{"log_level", log_level_params}, {NULL, NULL}};
+
+static MunitResult vary_log_level(const MunitParameter params[], void* data)
+{
+    struct test_context* ctx = (struct test_context*)data;
+    const char* level;
+    int ret;
+    margo_log_level mll = 0;
+
+    level = munit_parameters_get(params, "log_level");
+    if(strcmp("trace", level) == 0)
+        mll = MARGO_LOG_TRACE;
+    if(strcmp("debug", level) == 0)
+        mll = MARGO_LOG_DEBUG;
+    if(strcmp("info", level) == 0)
+        mll = MARGO_LOG_INFO;
+    if(strcmp("warning", level) == 0)
+        mll = MARGO_LOG_WARNING;
+    if(strcmp("error", level) == 0)
+        mll = MARGO_LOG_ERROR;
+    if(strcmp("critical", level) == 0)
+        mll = MARGO_LOG_CRITICAL;
+
+    ret = margo_set_log_level(ctx->mid, mll);
+    munit_assert_int(ret, ==, 0);
+
+    margo_trace(ctx->mid, "TRACE ");
+    margo_debug(ctx->mid, "DEBUG ");
+    margo_info(ctx->mid, "INFO ");
+    margo_warning(ctx->mid, "WARNING ");
+    margo_error(ctx->mid, "ERROR ");
+    margo_critical(ctx->mid, "CRITICAL ");
+
+    if(mll > MARGO_LOG_TRACE)
+        munit_assert_null(strstr(ctx->log_buffer, "TRACE"));
+    else
+        munit_assert_not_null(strstr(ctx->log_buffer, "TRACE"));
+    if(mll > MARGO_LOG_DEBUG)
+        munit_assert_null(strstr(ctx->log_buffer, "DEBUG"));
+    else
+        munit_assert_not_null(strstr(ctx->log_buffer, "DEBUG"));
+    if(mll > MARGO_LOG_INFO)
+        munit_assert_null(strstr(ctx->log_buffer, "INFO"));
+    else
+        munit_assert_not_null(strstr(ctx->log_buffer, "INFO"));
+    if(mll > MARGO_LOG_WARNING)
+        munit_assert_null(strstr(ctx->log_buffer, "WARNING"));
+    else
+        munit_assert_not_null(strstr(ctx->log_buffer, "WARNING"));
+    if(mll > MARGO_LOG_ERROR)
+        munit_assert_null(strstr(ctx->log_buffer, "ERROR"));
+    else
+        munit_assert_not_null(strstr(ctx->log_buffer, "ERROR"));
+    if(mll > MARGO_LOG_CRITICAL)
+        munit_assert_null(strstr(ctx->log_buffer, "CRITICAL"));
+    else
+        munit_assert_not_null(strstr(ctx->log_buffer, "CRITICAL"));
+
+    return MUNIT_OK;
+}
+
 static MunitTest tests[]
     = {{"/default_log_level", default_log_level, test_context_setup,
         test_context_tear_down, MUNIT_TEST_OPTION_NONE, NULL},
+       {"/vary_log_level", vary_log_level, test_context_setup,
+        test_context_tear_down, MUNIT_TEST_OPTION_NONE, get_log_level},
        {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}};
 
 static const MunitSuite test_suite
