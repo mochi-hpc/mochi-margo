f40565b33a40c27e35d4a336d06abf34039e4c15
Matthieu Dorier <mdorier@anl.gov>
2023-02-07 19:30:55 +0000

    use jenkins hash function for RPC id generation

--- src/margo-id.h
+++ src/margo-id.h
@@ -6,6 +6,8 @@
 #ifndef __MARGO_ID_H
 #define __MARGO_ID_H
 
+#include "lookup3.h"
+
 static inline void demux_id(hg_id_t in, hg_id_t* base_id, uint16_t* provider_id)
 {
     /* retrieve low bits for provider */
@@ -34,13 +36,15 @@ static inline hg_id_t mux_id(hg_id_t base_id, uint16_t provider_id)
 
 static inline hg_id_t gen_id(const char* func_name, uint16_t provider_id)
 {
-    hg_id_t  id;
-    unsigned hashval;
-
-    HASH_JEN(func_name, strlen(func_name), hashval);
-    id = hashval << (__MARGO_PROVIDER_ID_SIZE * 8);
+    hg_id_t id  = 0;
+    int32_t a32 = 0, b32 = 0;
+    ch_bj_hashlittle2(func_name, strlen(func_name), &a32, &b32);
+    b32          = b32 << (__MARGO_PROVIDER_ID_SIZE * 8);
+    uint64_t a64 = a32;
+    a64          = a64 << 32;
+    id |= a64;
+    id |= b32;
     id |= provider_id;
-
     return id;
 }
 
