ed45e29bb710c16293ef7440d7c53bbf7fcbedf8
Matthieu Dorier <mdorier@anl.gov>
2023-08-23 12:49:53 +0000

    improved unit test for ABT pools

--- tests/unit-tests/margo-abt-pool.c
+++ tests/unit-tests/margo-abt-pool.c
@@ -47,6 +47,8 @@ static int count_occurrence(const char* haystack, const char *needle)
     return(count);
 }
 
+static void thread_func(void*) {}
+
 /* test different ways of specifying different "kind" for
  * dedicated rpc handler pool
  */
@@ -58,7 +60,29 @@ static MunitResult rpc_pool_kind(const MunitParameter params[], void* data)
     char *runtime_config;
     int count;
 
-    mii.json_config = munit_parameters_get(params, "json");
+    const char* config_fmt = "{"
+        "\"rpc_thread_count\":0,"
+        "\"argobots\": {"
+            "\"pools\": ["
+                "{ \"name\":\"my_pool\", \"kind\":\"%s\" }"
+            "],"
+            "\"xstreams\": ["
+                "{ \"name\":\"my_xstream\", "
+                  "\"scheduler\": {"
+                    "\"type\":\"basic_wait\","
+                    "\"pools\":[\"my_pool\"]"
+                  "}"
+                "}"
+            "]"
+        "}"
+    "}";
+
+    const char* pool_kind = munit_parameters_get(params, "pool");
+
+    char config[4096] = {0};
+    sprintf(config, config_fmt, pool_kind);
+
+    mii.json_config = config;
 
     ctx->mid = margo_init_ext(protocol, MARGO_SERVER_MODE, &mii);
     munit_assert_not_null_goto(ctx->mid, error);
@@ -72,14 +96,24 @@ static MunitResult rpc_pool_kind(const MunitParameter params[], void* data)
     munit_assert_int_goto(count, ==, 1, error);
 
     /* just one pool with the prio_wait kind */
-    if(count_occurrence(mii.json_config, "prio_wait")) {
-        count = count_occurrence(runtime_config, "prio_wait");
-    } else if(count_occurrence(mii.json_config, "efirst_wait")) {
-        count = count_occurrence(runtime_config, "efirst_wait");
-    }
+    count = count_occurrence(runtime_config, pool_kind);
     munit_assert_int_goto(count, ==, 1, error);
 
     free(runtime_config);
+
+    struct margo_pool_info info = {0};
+    hg_return_t hret = margo_find_pool_by_name(ctx->mid, "my_pool", &info);
+    munit_assert_int_goto(hret, ==, HG_SUCCESS, error);
+
+    // try to post 4 ULTs to the pool
+    ABT_thread ults[4];
+    for(unsigned i=0; i < 4; ++i) {
+        ABT_thread_create(info.pool, thread_func, NULL, ABT_THREAD_ATTR_NULL, ults+i);
+    }
+    for(unsigned i=0; i < 4; ++i) {
+        ABT_thread_join(ults[i]);
+        ABT_thread_free(ults+i);
+    }
 
     margo_finalize(ctx->mid);
 
@@ -89,14 +123,19 @@ error:
     return MUNIT_FAIL;
 }
 
+static char* pool_params[] = {
+    "prio_wait",
+    "efirst_wait",
+    NULL
+};
+
 static char * json_params[] = {
-    "{ \"rpc_thread_count\":0, \"argobots\":{ \"pools\":[ { \"name\":\"my_pool\", \"kind\":\"prio_wait\" } ] } }",
     "{ \"rpc_thread_count\":0, \"argobots\":{ \"pools\":[ { \"name\":\"my_pool\", \"kind\":\"efirst_wait\" } ] } }",
     NULL
 };
 
 static MunitParameterEnum rpc_pool_kind_params[] = {
-    { "json", json_params},
+    { "pool", pool_params},
     {NULL, NULL}
 };
 
