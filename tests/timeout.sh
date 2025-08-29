f88b9b17eb82d958ca89c47becdb87168fb2cedc
Matthieu Dorier <mdorier@anl.gov>
2025-08-29 16:46:30 +0100

    adapted to cmake

--- tests/timeout.sh
+++ tests/timeout.sh
@@ -1,16 +1,8 @@
 #!/bin/bash -x
 
-if [ -z $srcdir ]; then
-    echo srcdir variable not set.
-    exit 1
-fi
+MKTEMP=${MKTEMP:-mktemp}
 
-if [ -z "$MKTEMP" ] ; then
-    echo expected MKTEMP variable defined to its respective command
-    exit 1
-fi
-
-source $srcdir/tests/test-util.sh
+source $(dirname $0)/test-util.sh
 
 TMPOUT=$($MKTEMP test-XXXXXX)
 
@@ -22,7 +14,7 @@ sleep 1
 #####################
 
 # run client test
-run_to 20 tests/margo-test-client-timeout $svr1 &> $TMPOUT 
+run_to 20 ./margo-test-client-timeout $svr1 &> $TMPOUT
 if [ $? -ne 0 ]; then
     wait
     rm -rf $TMPOUT
@@ -31,7 +23,7 @@ fi
 
 # check output; look for four "returned 18" to indicate HG_TIMEOUT in the four
 # concurrent RPCs
-LINECOUNT=$(grep "returned HG_TIMEOUT" $TMPOUT | wc -l) 
+LINECOUNT=$(grep "returned HG_TIMEOUT" $TMPOUT | wc -l)
 if [ $LINECOUNT -ne 4 ]; then
     rm -rf $TMPOUT
     exit 1
